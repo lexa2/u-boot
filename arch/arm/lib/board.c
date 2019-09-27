lexa2/u-boot

commit e7e4914e34de70f78b645745efd26a3cfdd0e0da

fix gcc7 build-errors

arch/arm/lib/board.c
@@ -70,24 +70,24 @@ extern void dataflash_print_info(void);
  * May be supplied by boards if desired
  */
 inline void __coloured_LED_init(void) {}
-void coloured_LED_init(void)
-	__attribute__((weak, alias("__coloured_LED_init")));
+//void coloured_LED_init(void)
+//	__attribute__((weak, alias("__coloured_LED_init")));
 inline void __red_led_on(void) {}
-void red_led_on(void) __attribute__((weak, alias("__red_led_on")));
+//void red_led_on(void) __attribute__((weak, alias("__red_led_on")));
 inline void __red_led_off(void) {}
-void red_led_off(void) __attribute__((weak, alias("__red_led_off")));
+//void red_led_off(void) __attribute__((weak, alias("__red_led_off")));
 inline void __green_led_on(void) {}
-void green_led_on(void) __attribute__((weak, alias("__green_led_on")));
+//void green_led_on(void) __attribute__((weak, alias("__green_led_on")));
 inline void __green_led_off(void) {}
-void green_led_off(void) __attribute__((weak, alias("__green_led_off")));
+//void green_led_off(void) __attribute__((weak, alias("__green_led_off")));
 inline void __yellow_led_on(void) {}
-void yellow_led_on(void) __attribute__((weak, alias("__yellow_led_on")));
+//void yellow_led_on(void) __attribute__((weak, alias("__yellow_led_on")));
 inline void __yellow_led_off(void) {}
-void yellow_led_off(void) __attribute__((weak, alias("__yellow_led_off")));
+//void yellow_led_off(void) __attribute__((weak, alias("__yellow_led_off")));
 inline void __blue_led_on(void) {}
-void blue_led_on(void) __attribute__((weak, alias("__blue_led_on")));
+//void blue_led_on(void) __attribute__((weak, alias("__blue_led_on")));
 inline void __blue_led_off(void) {}
-void blue_led_off(void) __attribute__((weak, alias("__blue_led_off")));
+//void blue_led_off(void) __attribute__((weak, alias("__blue_led_off")));
 
 /*
  ************************************************************************

common/main.c
@@ -28,7 +28,7 @@ DECLARE_GLOBAL_DATA_PTR;
  * Board-specific Platform code can reimplement show_boot_progress () if needed
  */
 void inline __show_boot_progress (int val) {}
-void show_boot_progress (int val) __attribute__((weak, alias("__show_boot_progress")));
+//void show_boot_progress (int val) __attribute__((weak, alias("__show_boot_progress")));
 
 #define MAX_DELAY_STOP_STR 32
 

arch/arm/include/asm/io.h
@@ -75,7 +75,7 @@ static inline phys_addr_t virt_to_phys(void * vaddr)
 #define __arch_putw(v,a)		(*(volatile unsigned short *)(a) = (v))
 #define __arch_putl(v,a)		(*(volatile unsigned int *)(a) = (v))
 
-extern inline void __raw_writesb(unsigned long addr, const void *data,
+static inline void __raw_writesb(unsigned long addr, const void *data,
 				 int bytelen)
 {
 	uint8_t *buf = (uint8_t *)data;
@@ -83,7 +83,7 @@ extern inline void __raw_writesb(unsigned long addr, const void *data,
 		__arch_putb(*buf++, addr);
 }
 
-extern inline void __raw_writesw(unsigned long addr, const void *data,
+static inline void __raw_writesw(unsigned long addr, const void *data,
 				 int wordlen)
 {
 	uint16_t *buf = (uint16_t *)data;
@@ -91,7 +91,7 @@ extern inline void __raw_writesw(unsigned long addr, const void *data,
 		__arch_putw(*buf++, addr);
 }
 
-extern inline void __raw_writesl(unsigned long addr, const void *data,
+static inline void __raw_writesl(unsigned long addr, const void *data,
 				 int longlen)
 {
 	uint32_t *buf = (uint32_t *)data;
@@ -99,21 +99,21 @@ extern inline void __raw_writesl(unsigned long addr, const void *data,
 		__arch_putl(*buf++, addr);
 }
 
-extern inline void __raw_readsb(unsigned long addr, void *data, int bytelen)
+static inline void __raw_readsb(unsigned long addr, void *data, int bytelen)
 {
 	uint8_t *buf = (uint8_t *)data;
 	while(bytelen--)
 		*buf++ = __arch_getb(addr);
 }
 
-extern inline void __raw_readsw(unsigned long addr, void *data, int wordlen)
+static inline void __raw_readsw(unsigned long addr, void *data, int wordlen)
 {
 	uint16_t *buf = (uint16_t *)data;
 	while(wordlen--)
 		*buf++ = __arch_getw(addr);
 }
 
-extern inline void __raw_readsl(unsigned long addr, void *data, int longlen)
+static inline void __raw_readsl(unsigned long addr, void *data, int longlen)
 {
 	uint32_t *buf = (uint32_t *)data;
 	while(longlen--)

board/mediatek/mt7622_evb/mt7622_evb.c
@@ -34,6 +34,12 @@ int dram_init(void)
 	return 0;
 }
 
+void show_boot_progress(int val) {}
+
+void coloured_LED_init(void) {}
+
+void red_led_on(void) {}
+
 int board_init(void)
 {
     mtk_timer_init();

commit 99209acfa512acf68a71dc7b04c88753404013d9

another way to fix weak-functions by moore liu
fixes: e7e4914e34de70f78b645745efd26a3cfdd0e0da

arch/arm/lib/board.c
@@ -70,24 +70,24 @@ extern void dataflash_print_info(void);
  * May be supplied by boards if desired
  */
 inline void __coloured_LED_init(void) {}
-//void coloured_LED_init(void)
-//	__attribute__((weak, alias("__coloured_LED_init")));
+void coloured_LED_init(void)
+	__attribute__((weak, alias("__coloured_LED_init")));
 inline void __red_led_on(void) {}
-//void red_led_on(void) __attribute__((weak, alias("__red_led_on")));
+void red_led_on(void) __attribute__((weak, alias("__red_led_on")));
 inline void __red_led_off(void) {}
-//void red_led_off(void) __attribute__((weak, alias("__red_led_off")));
+void red_led_off(void) __attribute__((weak, alias("__red_led_off")));
 inline void __green_led_on(void) {}
-//void green_led_on(void) __attribute__((weak, alias("__green_led_on")));
+void green_led_on(void) __attribute__((weak, alias("__green_led_on")));
 inline void __green_led_off(void) {}
-//void green_led_off(void) __attribute__((weak, alias("__green_led_off")));
+void green_led_off(void) __attribute__((weak, alias("__green_led_off")));
 inline void __yellow_led_on(void) {}
-//void yellow_led_on(void) __attribute__((weak, alias("__yellow_led_on")));
+void yellow_led_on(void) __attribute__((weak, alias("__yellow_led_on")));
 inline void __yellow_led_off(void) {}
-//void yellow_led_off(void) __attribute__((weak, alias("__yellow_led_off")));
+void yellow_led_off(void) __attribute__((weak, alias("__yellow_led_off")));
 inline void __blue_led_on(void) {}
-//void blue_led_on(void) __attribute__((weak, alias("__blue_led_on")));
+void blue_led_on(void) __attribute__((weak, alias("__blue_led_on")));
 inline void __blue_led_off(void) {}
-//void blue_led_off(void) __attribute__((weak, alias("__blue_led_off")));
+void blue_led_off(void) __attribute__((weak, alias("__blue_led_off")));
 
 /*
  ************************************************************************

common/main.c
@@ -28,7 +28,7 @@ DECLARE_GLOBAL_DATA_PTR;
  * Board-specific Platform code can reimplement show_boot_progress () if needed
  */
 void inline __show_boot_progress (int val) {}
-//void show_boot_progress (int val) __attribute__((weak, alias("__show_boot_progress")));
+void show_boot_progress (int val) __attribute__((weak, alias("__show_boot_progress")));
 
 #define MAX_DELAY_STOP_STR 32
 

include/linux/compiler-gcc.h
@@ -49,6 +49,10 @@
 # define __inline__	__inline__	__attribute__((always_inline))
 # define __inline	__inline	__attribute__((always_inline))
 */
+/* XXX: check __GNUC_STDC_INLINE__, fix line length */
+# define inline                inline          __attribute__((always_inline)) __attribute__((__gnu_inline__))
+# define __inline__    __inline__      __attribute__((always_inline)) __attribute__((__gnu_inline__))
+# define __inline      __inline        __attribute__((always_inline)) __attribute__((__gnu_inline__))
 #endif
 
 #define __deprecated			__attribute__((deprecated))