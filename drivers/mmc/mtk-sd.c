141c8080989d5f685af2498989b2f215aa0e5883
Oleksandr Rybalko <ray@ddteam.net>
2019-10-28 09:57:40 +0200

    save in very dirty state

--- drivers/mmc/mtk-sd.c
+++ drivers/mmc/mtk-sd.c
@@ -1276,7 +1276,7 @@ static void msdc_init_hw(struct msdc_host *host)
 
 static void msdc_ungate_clock(struct msdc_host *host)
 {
-#if 0
+#if 1
 	clk_enable(&host->src_clk);
 	clk_enable(&host->h_clk);
 	if (host->src_clk_cg.dev)
@@ -1295,7 +1295,7 @@ static int msdc_drv_probe(struct udevice *dev)
 
 	host->dev_comp = (struct msdc_compatible *)dev_get_driver_data(dev);
 
-#if 0
+#if 1
 	host->src_clk_freq = clk_get_rate(&host->src_clk);
 #else
 	host->src_clk_freq = 400000000;
@@ -1340,7 +1340,7 @@ static int msdc_ofdata_to_platdata(struct udevice *dev)
 	if (ret)
 		return ret;
 
-#if 0
+#if 1
 	ret = clk_get_by_name(dev, "source", &host->src_clk);
 	if (ret < 0)
 		return ret;
