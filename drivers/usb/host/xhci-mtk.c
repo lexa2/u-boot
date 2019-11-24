f4d7693ffd30a28dde2f41758eaed27f34f200b0
Frank Wunderlich <frank-w@public-files.de>
2019-11-24 12:34:32 +0100

    xhci: try to add regulators

--- drivers/usb/host/xhci-mtk.c
+++ drivers/usb/host/xhci-mtk.c
@@ -17,8 +17,11 @@
 #include <linux/compat.h>
 #include <linux/usb/dwc3.h>
 #include <power/regulator.h>
+#include <asm/gpio.h>
 
 #include <usb/xhci.h>
+
+
 
 struct mtk_xhci {
 	/*struct usb_platdata usb_plat;
@@ -27,9 +30,87 @@ struct mtk_xhci {
 	struct dwc3 *dwc3_reg;*/
 };
 
+struct mtk_xhci_platdata { // xhci_hcd_mtk
+        struct device *dev;
+        struct udevice *vusb33;
+        struct udevice *vbus;
+};
+/*
+struct xhci_hcd_mtk {
+        struct device *dev;
+        struct usb_hcd *hcd;
+        struct mu3h_sch_bw_info *sch_array;
+        struct mu3c_ippc_regs __iomem *ippc_regs;
+        bool has_ippc;
+        int num_u2_ports;
+        int num_u3_ports;
+        int u3p_dis_msk;
+        struct regulator *vusb33;
+        struct regulator *vbus;
+        struct clk *sys_clk;    // sys and mac clock
+        struct clk *xhci_clk;
+        struct clk *ref_clk;
+        struct clk *mcu_clk;
+        struct clk *dma_clk;
+        struct regmap *pericfg;
+        struct phy **phys;
+        int num_phys;
+        bool lpm_support;
+        // usb remote wakeup
+        bool uwk_en;
+        struct regmap *uwk;
+        u32 uwk_reg_base;
+        u32 uwk_vers;
+};
+*/
+
 static int mtk_xhci_probe(struct udevice *dev)
 {
+	struct mtk_xhci_platdata *mtk = dev_get_platdata(dev);
+	struct udevice *regulator,*regulator2;
+
+	int ret;
+
 	printf("%s %d",__FUNCTION__,__LINE__);
+
+
+/*
+        mtk = devm_kzalloc(dev, sizeof(*mtk), GFP_KERNEL);
+        if (!mtk)
+                return -ENOMEM;
+
+        mtk->dev = dev;
+
+        mtk->vbus = devm_regulator_get(dev, "vbus");
+        if (IS_ERR(mtk->vbus)) {
+                dev_err(dev, "fail to get vbus\n");
+                return PTR_ERR(mtk->vbus);
+        }
+
+        mtk->vusb33 = devm_regulator_get(dev, "vusb33");
+        if (IS_ERR(mtk->vusb33)) {
+                dev_err(dev, "fail to get vusb33\n");
+                return PTR_ERR(mtk->vusb33);
+        }
+
+        ret = xhci_mtk_clks_get(mtk);
+        if (ret)
+                return ret;
+*/
+
+        ret = device_get_supply_regulator(dev, "vbus", &mtk->vbus);
+        if (!ret) {
+		//mtk->vbus = regulator;
+                ret = regulator_set_enable(mtk->vbus, true);
+                if (ret) {}
+	}else printf ("cannot get vbus\n");
+
+        ret = device_get_supply_regulator(dev, "vusb33", &mtk->vusb33);
+        if (!ret) {
+                ret = regulator_set_enable(mtk->vusb33, true);
+                if (ret) {}
+	}else printf("cannot get vusb33");
+
 /*
 	struct rockchip_xhci_platdata *plat = dev_get_platdata(dev);
 	struct rockchip_xhci *ctx = dev_get_priv(dev);
@@ -71,5 +152,6 @@ U_BOOT_DRIVER(mtk_xhci) = {
 	.of_match	= mtk_xhci_id_table,
 	//.ops		= &xhci_usb_ops,
 	.probe		= mtk_xhci_probe,
+	.platdata_auto_alloc_size = sizeof(struct mtk_xhci_platdata),
 	.priv_auto_alloc_size = sizeof(struct mtk_xhci),
 };
